b99855c93af0bb81ed57f28e2d0454a2102dd5d9
Harsh Raj Ranjan <harshrajranjan2005@gmail.com>
2025-09-18 00:25:45 +0530

    Update vite.config.ts
    updated base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // ✅ <-- Add this line for GitHub Pages
+
   server: {
     host: "::",
     port: 8080,
